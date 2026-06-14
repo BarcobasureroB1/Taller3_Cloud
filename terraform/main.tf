e07e1a1372f8a7005d425b12f4c675d42cf6b982
Benjamín Garcés <bgarces302@gmail.com>
2026-06-14 05:58:42 -0400

    🐛fix(terraform): Modificación main.tf
    arreglo de funcionamiento para versión actualizada

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,10 @@ provider "aws" {
   access_key = "test"
   secret_key = "test"
 
-
-  s3_use_path_style = true
-
-
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  skip_requested_account_id_validation = true
+  skip_requesting_account_id  = true
 
   endpoints {
     s3 = var.localstack_endpoint
